46a314b6bc23b9a3feb1437c12fb24fd3177823f
Pritpal Singh <psingh50@mail.greenriver.edu>
2017-05-17 21:40:46 -0500

    Created the field and methods of BlogPost class and Blogger class

--- classes/blog_post.php
+++ classes/blog_post.php
@@ -1,6 +1,46 @@
 <?php
 	class BlogPost
 	{
+		private $_title;
+		private $_blog_entry;
+		private $_word_count;
+		private $_created_date;
+		
+		public function __construct($title, $blogEntry) {
+			$this->_title = $title;
+		}
+		
+		public function setTitle($title) {
+			$this->_title = $title;
+		}
+		
+		public function setBlogEntry($blogEntry) {
+			$this->_blog_entry = $blogEntry;
+		}
+		
+		public function setWordCount($count) {
+			$this->_word_count = $count;
+		}
+		
+		public function setCreatedDate($date) {
+			$this->_created_date = $date;
+		}
+		
+		public function getTitle() {
+			return $this->_title;
+		}
+		
+		public function getBlogEntry() {
+			return $this->_blog_entry;
+		}
+		
+		public function getWordCount() {
+			return 10;
+		}
+		
+		public function getCreatedDate() {
+			return $this->_created_date''
+		}
 		
 	}
 ?>
--- classes/blogger.php
+++ classes/blogger.php
@@ -1,6 +1,74 @@
 <?php
 	class Blogger
 	{
+		private $_username;
+		private $_email;
+		private $_password;
+		private $_portrait;
+		private $_bio;
+		private $_blog_count;
+		private $_excerpt;
 		
+		public function __construct($username, $email, $password) {
+			$this->_username = $username;
+			$this->_email = $email;
+			$this->_password = $password;
+		}
+		
+		public function setUsername($username) {
+			$this->_username = $username;
+		}
+		
+		public function setEmail($email) {
+			$this->_email = $email;
+		}
+		
+		public function setPassword($password) {
+			$this->_password = $password;
+		}
+		
+		public function setPortrait($portrait) {
+			$this->_portrait = $portrait;
+		}
+		
+		public function setBio($bio) {
+			$this->_bio = $bio;
+		}
+		
+		public function setBlogCount($count) {
+			$this->setBlogCount = $count;
+		}
+		
+		public function setExcerpt($excerpt) {
+			$this->setExcerpt = $excerpt;
+		}
+		
+		public function getUsername() {
+			return $this->_username;
+		}
+		
+		public function getEmail() {
+			return $this->_email;
+		}
+		
+		public function getPassword() {
+			return return $this->_password;
+		}
+		
+		public function getPortrait() {
+			return $this->_portrait;
+		}
+		
+		public function getBio() {
+			return $this->_bio;
+		}
+		
+		public function getBlogCount() {
+			return $this->_blog_count;
+		}
+		
+		public function getExcerpt() {
+			return $this->_excerpt;
+		}
 	}
 ?>
