7a3dbc7e2ee135ff2538714f0db212f73420f24c
Pritpal Singh <psingh50@mail.greenriver.edu>
2017-05-20 17:47:19 -0500

    Defined routes for Login page, signup page, home page, and about us page.

--- index.php
+++ index.php
@@ -21,28 +21,24 @@
 		$view = new View;
 		echo $view->render('pages/home.html');
 	});
-	/**
-	 * This is the helper function to move the uploaded profile photo to
-	 * a permanent location.
-	 *
-	 * @return string The name of the uploaded photo if upload was successful
-	 * and the name of the default profile photo otherwise.
-	*/
-	function processUploadedPhoto() {
-		//Only accept jpeg, png, or jpg file types
-		if (isset($_FILES["profilePhoto"]) and $_FILES["profilePhoto"]["error"] == UPLOAD_ERR_OK) {
-			if ($_FILES["profilePhoto"]["type"] != "image/jpeg" && $_FILES["profilePhoto"]["type"] != "image/png"
-				&& $_FILES["profilePhoto"]["type"] != "image/jpg") {
-				//return the location and name of the default profile photo
-				return "profile.png";
-			}
-			if (move_uploaded_file($_FILES["profilePhoto"]["tmp_name"],
-								   "images/" . basename($_FILES["profilePhoto"]["name"]))) {
-				//return the location and name of the uploaded photo
-				return basename($_FILES["profilePhoto"]["name"]);
-			}
-		}
-		//return the name of the default profile photo
-		return "profile.png";
-	}
+	
+	//Login Page
+	$f3->route('GET /login', function(){
+		$view = new View;
+		echo $view->render('pages/login.html');
+	});
+	
+	//Signup Page
+	$f3->route('GET /sign-up', function() {
+		$view = new View;
+		echo $view->render('pages/sign_up.html');
+	});
+	
+	//About Us Page
+	$f3->route('GET /about-us', function() {
+		$view = new View;
+		echo $view->render('pages/about_us.html');
+	});
+	
+	$f3->run();
 ?>
